5bb219d0ca37e3c50006ed905353bc28b735aa97
Ryzzen-mar <martin.decultot@orange.fr>
2022-04-04 14:09:09 +0200

    barre de navigation

--- app/src/main/java/fr/martin/weparty/MainActivity.java
+++ app/src/main/java/fr/martin/weparty/MainActivity.java
@@ -1,8 +1,13 @@
 package fr.martin.weparty;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView barreNav = findViewById(R.id.bottomNavigationView2);
+        barreNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch(item.getItemId()) {
+
+                        case R.id.home_nav:
+                            selectedFragment = new HomeFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+                    return true;
+                }
+            };
 }
