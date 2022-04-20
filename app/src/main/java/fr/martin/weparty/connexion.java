f6066b8bfe1014e96cceef30c217ce893ec09b83
Ryzzen-mar <martin.decultot@orange.fr>
2022-04-20 16:05:22 +0200

    nom utilisateur et mot de passe récupérés sur la page connexion

--- app/src/main/java/fr/martin/weparty/connexion.java
+++ app/src/main/java/fr/martin/weparty/connexion.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class connexion extends AppCompatActivity {
@@ -14,6 +16,8 @@ public class connexion extends AppCompatActivity {
     private Button creerBouton;
     private Button connexionBouton;
     private TextView lien;
+    private EditText nomUtilisateurConnexion;
+    private EditText motPasseConnexion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,8 @@ public class connexion extends AppCompatActivity {
 
         creerBouton = (Button) findViewById(R.id.bout_creer_compte);
         connexionBouton = (Button) findViewById(R.id.bouton_connexion);
+        nomUtilisateurConnexion   = (EditText)findViewById(R.id.nom_utilisateur);
+        motPasseConnexion = (EditText)findViewById(R.id.mot_de_passe);
         TextView lien = findViewById(R.id.mot_oublie);
 
         lien.setMovementMethod(LinkMovementMethod.getInstance());
@@ -37,10 +43,16 @@ public class connexion extends AppCompatActivity {
 
         connexionBouton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent menuActivity = new Intent (getApplicationContext(), MainActivity.class);
-                menuActivity.putExtra("activity","first");
-                startActivity(menuActivity);
-                finish();
+                String nomUtilisateurConnexionStr=nomUtilisateurConnexion.getText().toString();
+                String motPasseConnexionStr=motPasseConnexion.getText().toString();
+                System.out.println(nomUtilisateurConnexionStr);
+                System.out.println(motPasseConnexionStr);
+                //Si nom utilisateur correspond à celui dans BDD et si mot de passe correspond {
+                    Intent menuActivity = new Intent (getApplicationContext(), MainActivity.class);
+                    menuActivity.putExtra("activity","first");
+                    startActivity(menuActivity);
+                    finish();
+                //}
             }
         });
 
