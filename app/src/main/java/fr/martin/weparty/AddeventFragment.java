c9e90170d602041432c25824c29437516d4b1a7d
Ryzzen-mar <martin.decultot@orange.fr>
2022-04-21 15:39:21 +0200

    s'enregistrer renvoie sur connexion

--- app/src/main/java/fr/martin/weparty/AddeventFragment.java
+++ app/src/main/java/fr/martin/weparty/AddeventFragment.java
@@ -1,9 +1,13 @@
 package fr.martin.weparty;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +15,39 @@ import androidx.fragment.app.Fragment;
 
 public class AddeventFragment extends Fragment {
 
+    private Button envoyerEvenementButton;
+    private EditText titreEvenement;
+    private EditText descEvenement;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.creation_evenement,container,false);
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        envoyerEvenementButton = (Button) view.findViewById(R.id.envoyer_evenement);
+        titreEvenement   = (EditText) view.findViewById(R.id.titre_evenement);
+        descEvenement = (EditText) view.findViewById(R.id.desc_evenement);
+
+
+        envoyerEvenementButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String titreEvenementStr=titreEvenement.getText().toString();
+                String descEvenementStr=descEvenement.getText().toString();
+
+                //Si nom utilisateur correspond à celui dans BDD et si mot de passe correspond {
+//                Intent menuActivity = new Intent (getApplicationContext(), MainActivity.class);
+//                menuActivity.putExtra("activity","first");
+//                startActivity(menuActivity);
+//                finish();
+                //}
+            }
+        });
+
+    }
+
+
 }
--- app/src/main/java/fr/martin/weparty/creerCompte.java
+++ app/src/main/java/fr/martin/weparty/creerCompte.java
@@ -2,13 +2,45 @@ package fr.martin.weparty;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class creerCompte extends AppCompatActivity {
+    private EditText nomUtilisateurCreer;
+    private EditText motPasseCreer;
+    private EditText confirmerMotPasse;
+    private Button creerButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creer_compte);
+
+
+        nomUtilisateurCreer   = (EditText)findViewById(R.id.nom_utilisateur_creer);
+        motPasseCreer = (EditText)findViewById(R.id.mot_de_passe_compte);
+        confirmerMotPasse = (EditText)findViewById(R.id.confirmer_mot_passe);
+        creerButton = (Button)findViewById(R.id.creer_botton);
+
+
+        creerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String nomUtilisateurCreerStr=nomUtilisateurCreer.getText().toString();
+                String motPasseCreerStr=motPasseCreer.getText().toString();
+                String confirmerMotPasseStr=confirmerMotPasse.getText().toString();
+
+                //Si nom utilisateur correspond à celui dans BDD et si mot de passe correspond {
+                    Intent connexion = new Intent (getApplicationContext(), connexion.class);
+                    //connexion.putExtra("activity","first");
+                    startActivity(connexion);
+                    finish();
+                //}
+            }
+        });
+
     }
 }
