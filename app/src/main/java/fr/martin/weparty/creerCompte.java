331113b9a1a52f2a795c53017d69330364fb593e
Ryzzen-mar <martin.decultot@orange.fr>
2022-04-22 15:46:53 +0200

    version avant kalani

--- app/src/main/java/fr/martin/weparty/creerCompte.java
+++ app/src/main/java/fr/martin/weparty/creerCompte.java
@@ -7,18 +7,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import com.google.firebase.ktx.Firebase;
 
 public class creerCompte extends AppCompatActivity {
     private EditText nomUtilisateurCreer;
     private EditText motPasseCreer;
     private EditText confirmerMotPasse;
     private Button creerButton;
+    private FirebaseAuth mAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creer_compte);
+        mAuth = FirebaseAuth.getInstance();
 
 
         nomUtilisateurCreer   = (EditText)findViewById(R.id.nom_utilisateur_creer);
