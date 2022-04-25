7b0d987b21581ca60f8d98d63aefafb0a24d449c
KCHANFOUR <chanfour.kalani@outlook.fr>
2022-04-25 18:52:51 +0200

    Ajout de données à la database

--- app/src/main/java/fr/martin/weparty/creerCompte.java
+++ app/src/main/java/fr/martin/weparty/creerCompte.java
@@ -15,6 +15,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.auth.User;
+
+import java.util.Collection;
 
 public class creerCompte extends AppCompatActivity {
 
@@ -22,6 +27,9 @@ public class creerCompte extends AppCompatActivity {
     private EditText motPasseCreer;
     private EditText confirmerMotPasse;
     private Button creerButton;
+
+
+    FirebaseFirestore db;
 
     FirebaseAuth mAuth;
 
@@ -31,44 +39,46 @@ public class creerCompte extends AppCompatActivity {
         setContentView(R.layout.activity_creer_compte);
 
 
-        nomUtilisateurCreer   = (EditText)findViewById(R.id.nom_utilisateur_creer);
-        motPasseCreer = (EditText)findViewById(R.id.mot_de_passe_compte);
-        confirmerMotPasse = (EditText)findViewById(R.id.confirmer_mot_passe);
-        creerButton = (Button)findViewById(R.id.creer_botton);
+        nomUtilisateurCreer = (EditText) findViewById(R.id.nom_utilisateur_creer);
+        motPasseCreer = (EditText) findViewById(R.id.mot_de_passe_compte);
+        confirmerMotPasse = (EditText) findViewById(R.id.confirmer_mot_passe);
+        creerButton = (Button) findViewById(R.id.creer_botton);
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
 
         creerButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                addtoDB();
                 createUser();
-                Intent connexionActivity = new Intent (getApplicationContext(), connexion.class);
+                Intent connexionActivity = new Intent(getApplicationContext(), connexion.class);
                 startActivity(connexionActivity);
                 finish();
             }
         });
     }
 
-    private void createUser(){
+    public void createUser() {
         String email = nomUtilisateurCreer.getText().toString();
         String password = motPasseCreer.getText().toString();
-        String passwordverif =  confirmerMotPasse.getText().toString();
+        String passwordverif = confirmerMotPasse.getText().toString();
 
-        if (TextUtils.isEmpty(email)){
+        if (TextUtils.isEmpty(email)) {
             nomUtilisateurCreer.setError("Email cannot be empty");
             nomUtilisateurCreer.requestFocus();
-        }else if (TextUtils.isEmpty(password)){
+        } else if (TextUtils.isEmpty(password)) {
             motPasseCreer.setError("Password cannot be empty");
             motPasseCreer.requestFocus();
-        }else if (TextUtils.isEmpty(passwordverif)) {
+        } else if (TextUtils.isEmpty(passwordverif)) {
             confirmerMotPasse.setError("Password cannot be empty");
             confirmerMotPasse.requestFocus();
-        }else{
-            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        } else {
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()){
+                    if (task.isSuccessful()) {
                         Toast.makeText(creerCompte.this, "User registered successfully", Toast.LENGTH_SHORT).show();
                         startActivity(new Intent(creerCompte.this, connexion.class));
-                    }else{
+                    } else {
                         Toast.makeText(creerCompte.this, "Registration Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 }
@@ -76,4 +86,34 @@ public class creerCompte extends AppCompatActivity {
         }
     }
 
+    private void addtoDB() {
+
+        final String email = nomUtilisateurCreer.getText().toString();
+        final String password = motPasseCreer.getText().toString();
+        final String passwordverif = confirmerMotPasse.getText().toString();
+
+        if (email.isEmpty()) {
+            nomUtilisateurCreer.setError("Email cannot be empty");
+            return;
+        } else if (password.isEmpty()) {
+            motPasseCreer.setError("Password cannot be empty");
+            return;
+        } else if (passwordverif.isEmpty()) {
+            confirmerMotPasse.setError("Password cannot be empty");
+            return;
+        } else {
+            CollectionReference colRef = db.collection("Users");
+            Users user = new Users(email, password, passwordverif);
+            colRef.document().set(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(getApplicationContext(), "Data has been successfully added to the database", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
 }
