9a5ee8e13ec8a792d55108c59de6d9b6489d74b1
KCHANFOUR <chanfour.kalani@outlook.fr>
2022-05-04 20:02:06 +0200

    Ajout d'image à partir de la galerie du téléphone (manque à ajouter l'image à firestore)

--- app/src/main/java/fr/martin/weparty/AddeventFragment.java
+++ app/src/main/java/fr/martin/weparty/AddeventFragment.java
@@ -1,17 +1,37 @@
 package fr.martin.weparty;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.security.GeneralSecurityException;
+import java.util.UUID;
+
+import fr.martin.weparty.databinding.ActivityMainBinding;
+
 
 public class AddeventFragment extends Fragment {
 
@@ -21,14 +41,21 @@ public class AddeventFragment extends Fragment {
     private EditText dateEvenement;
     private EditText lieuEvenement;
     private EditText descEvenement;
+    private ImageView image;
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
+    Uri ImageUri;
+    FirebaseFirestore db;
+    ActivityMainBinding binding;
+    ActivityResultLauncher<String> mGetContent;
+
     //--------
 
-    @Nullable
     @Override
 
     //affichage du layout---------
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.creation_evenement,container,false);
+        return inflater.inflate(R.layout.creation_evenement, container, false);
     }
     //--------
 
@@ -37,35 +64,113 @@ public class AddeventFragment extends Fragment {
 
         //instanciation des variables------
         envoyerEvenementButton = (Button) view.findViewById(R.id.envoyer_evenement);
-        titreEvenement   = (EditText) view.findViewById(R.id.titre_evenement);
+        titreEvenement = (EditText) view.findViewById(R.id.titre_evenement);
         dateEvenement = (EditText) view.findViewById(R.id.date);
         lieuEvenement = (EditText) view.findViewById(R.id.lieu);
         descEvenement = (EditText) view.findViewById(R.id.desc_evenement);
+        image = (ImageView) view.findViewById(R.id.imageView);
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        db = FirebaseFirestore.getInstance();
         //--------
 
+        mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
+                new ActivityResultCallback<Uri>() {
+                    @Override
+                    public void onActivityResult(Uri result) {
+                        if (result != null) {
+                            ImageUri = result;
+                        }
+                    }
+                });
+
+
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mGetContent.launch("image/*");
+                uploadPicture();
+            }
+        });
 
         //Quand tu cliques sur le bouton envoyer de la page creation_evenement...------
         envoyerEvenementButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-
-                //récupération des valeurs des champs et conversion en strings
-                String titreEvenementStr=titreEvenement.getText().toString();
-                String dateEvenementStr=dateEvenement.getText().toString();
-                String lieuEvenementStr=lieuEvenement.getText().toString();
-                String descEvenementStr=descEvenement.getText().toString();
-                //-------
-
-
-                Intent menuActivity = new Intent (getActivity(), MainActivity.class);//instanciation de la page visée
-                menuActivity.putExtra("activity","first");//IMPORTANT, utilité dans MainActivity(lui permet de
+                try {
+                    addtoDB();
+                } catch (GeneralSecurityException e) {
+                    e.printStackTrace();
+                }
+                Intent menuActivity = new Intent(getActivity(), MainActivity.class);//instanciation de la page visée
+                menuActivity.putExtra("activity", "first");//IMPORTANT, utilité dans MainActivity(lui permet de
                 //connaitre l'activité précédente)
                 startActivity(menuActivity);//départ vers la page visée
 
             }
         });
-        //----------
-
     }
 
+    private void uploadPicture() {
 
+        final String randomkey = UUID.randomUUID().toString();
+        StorageReference riversRef = storageReference.child("image/*" + randomkey);
+        riversRef.putFile(ImageUri)
+                .addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        Toast.makeText(getActivity().getApplicationContext(), "Image téléchargée", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getActivity().getApplicationContext(), "Erreur", Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    public void addtoDB() throws GeneralSecurityException {
+
+        //récupération des valeurs des champs et conversion en strings
+        String titreEvenementStr = titreEvenement.getText().toString();
+        String dateEvenementStr = dateEvenement.getText().toString();
+        String lieuEvenementStr = lieuEvenement.getText().toString();
+        String descEvenementStr = descEvenement.getText().toString();
+        //-------
+
+        if (titreEvenementStr.isEmpty()) {
+            titreEvenement.setError("Ce champ ne peut pas être vide");
+            titreEvenement.requestFocus();
+
+        } else if (dateEvenementStr.isEmpty()) {
+            dateEvenement.setError("Ce champ ne peut pas être vide");
+            dateEvenement.requestFocus();
+
+        } else if (lieuEvenementStr.isEmpty()) {
+            lieuEvenement.setError("Ce champ ne peut pas être vide");
+            lieuEvenement.requestFocus();
+
+        } else if (descEvenementStr.isEmpty()) {
+            descEvenement.setError("Ce champ ne peut pas être vide");
+            descEvenement.requestFocus();
+        } else {
+            CollectionReference colRef = db.collection("Evenements");
+            Evenement Event = new Evenement(titreEvenementStr, dateEvenementStr, lieuEvenementStr, descEvenementStr);
+            colRef.document().set(Event).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(getActivity().getApplicationContext(), "L'événement a bien été ajouté", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getActivity().getApplicationContext(), "Erreur" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
 }
+
+
+
--- app/src/main/java/fr/martin/weparty/creerCompte.java
+++ app/src/main/java/fr/martin/weparty/creerCompte.java
@@ -1,25 +1,24 @@
 package fr.martin.weparty;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.method.LinkMovementMethod;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.text.TextUtils;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.auth.User;
 
-import java.util.Collection;
+import java.security.GeneralSecurityException;
 
 public class creerCompte extends AppCompatActivity {
 
@@ -27,7 +26,6 @@ public class creerCompte extends AppCompatActivity {
     private EditText motPasseCreer;
     private EditText confirmerMotPasse;
     private Button creerButton;
-
 
     FirebaseFirestore db;
 
@@ -48,7 +46,11 @@ public class creerCompte extends AppCompatActivity {
 
         creerButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                addtoDB();
+                try {
+                    addtoDB();
+                } catch (GeneralSecurityException e) {
+                    e.printStackTrace();
+                }
                 createUser();
                 Intent connexionActivity = new Intent(getApplicationContext(), connexion.class);
                 startActivity(connexionActivity);
@@ -63,30 +65,30 @@ public class creerCompte extends AppCompatActivity {
         String passwordverif = confirmerMotPasse.getText().toString();
 
         if (TextUtils.isEmpty(email)) {
-            nomUtilisateurCreer.setError("Email cannot be empty");
+            nomUtilisateurCreer.setError("Ce champ ne peut pas être vide");
             nomUtilisateurCreer.requestFocus();
         } else if (TextUtils.isEmpty(password)) {
-            motPasseCreer.setError("Password cannot be empty");
+            motPasseCreer.setError("Ce champ ne peut pas être vide");
             motPasseCreer.requestFocus();
         } else if (TextUtils.isEmpty(passwordverif)) {
-            confirmerMotPasse.setError("Password cannot be empty");
+            confirmerMotPasse.setError("Ce champ ne peut pas être vide");
             confirmerMotPasse.requestFocus();
         } else {
             mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()) {
-                        Toast.makeText(creerCompte.this, "User registered successfully", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(creerCompte.this, "Le compte a bien été créé", Toast.LENGTH_SHORT).show();
                         startActivity(new Intent(creerCompte.this, connexion.class));
                     } else {
-                        Toast.makeText(creerCompte.this, "Registration Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(creerCompte.this, "Erreur" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 }
             });
         }
     }
 
-    private void addtoDB() {
+    public void addtoDB() throws GeneralSecurityException {
 
         final String email = nomUtilisateurCreer.getText().toString();
         final String password = motPasseCreer.getText().toString();
@@ -94,16 +96,19 @@ public class creerCompte extends AppCompatActivity {
 
         if (email.isEmpty()) {
             nomUtilisateurCreer.setError("Email cannot be empty");
-            return;
+            nomUtilisateurCreer.requestFocus();
+
         } else if (password.isEmpty()) {
             motPasseCreer.setError("Password cannot be empty");
-            return;
+            motPasseCreer.requestFocus();
+
         } else if (passwordverif.isEmpty()) {
             confirmerMotPasse.setError("Password cannot be empty");
-            return;
+            confirmerMotPasse.requestFocus();
+
         } else {
             CollectionReference colRef = db.collection("Users");
-            Users user = new Users(email, password, passwordverif);
+            Utilisateur user = new Utilisateur(email);
             colRef.document().set(user).addOnCompleteListener(new OnCompleteListener<Void>() {
                 @Override
                 public void onComplete(@NonNull Task<Void> task) {
